2af9cbfaa01f8ffe63e1140792a2c942a71cd0b7
jereprz94 <jereprz1994@gmail.com>
2016-06-27 16:20:01 -0300

    closes #21, closes #22, vincule evento al boton tirar donde genera un numero aleatorio y lo compara con las apuestas de cada jugador

--- Ruleta/js/comportamiento.js
+++ Ruleta/js/comportamiento.js
@@ -14,18 +14,25 @@ function jugador(){
       this.dinero += variacion;
     },
     getapuesta : function() {
-      return this.apuesta;
+      return this.apuestas;
     },
     añadirapuesta : function(num){
       this.apuestas[num]++;
+    },
+    vaciarapuesta : function(){
+      this.apuestas = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
     }
   }
 };
 
 function numero(val){
   var rojos = $(".rojo");
+  var negros= $(".negro");
   return{
     valor : val,
+    getvalor : function(){
+      return this.valor;
+    },
     espar : function(){
       if (this.valor !=0){
         if (this.valor % 2 === 0) {
@@ -53,20 +60,23 @@ function numero(val){
       }
     },
     esrojo : function(){
-      if (this.valor !=0) {
+      var es = "";
+      if (this.valor != 0) {
         for (var i = 0; i < rojos.length; i++) {
-          if (rojos[i].innerHTML === this.valor) {
-            return true;
+          if (rojos[i].innerHTML == this.valor) {
+            es = true;
+            return es;
           }
           else {
-            return false;
+            es= false;
           }
         }
       }
       else {
         return null;
       }
-    }
+      return es;
+    },
   }
 };
 
@@ -82,10 +92,10 @@ function tablero(){
 
 /*-------------------------------------------------------------------------creacion funciones con acciones--------------------------------------------------*/
 
-function mostrar (){ /*muestra la apuesta y el dinero que va quedando*/
+function mostrar (jugador){ /*muestra la apuesta y el dinero que va quedando*/
     var apostado = "";
-    for (var i = 0; i < jugadoractual.apuestas.length; i++) {
-      if (jugadoractual.apuestas[i] != 0) {
+    for (var i = 0; i < jugador.apuestas.length; i++) {
+      if (jugador.apuestas[i] != 0) {
         if (i > 10) {
           var nomap = "";
           if (i === 11) {
@@ -106,28 +116,33 @@ function mostrar (){ /*muestra la apuesta y el dinero que va quedando*/
           else if (i === 16) {
             nomap = "rojo";
           }
-          apostado += nomap + '= $' + jugadoractual.apuestas[i] + '<br />';
+          apostado += nomap + '= $' + jugador.apuestas[i] + '<br />';
         }
         else {
-          apostado += i + '= $' + jugadoractual.apuestas[i] + '<br />';
+          apostado += i + '= $' + jugador.apuestas[i] + '<br />';
         }
       }
     }
-    if (jugadoractual === Mario) {
-      $("#dineroj1").html("Dinero: $" + jugadoractual.getdinero());
+    if (jugador === Mario) {
+      $("#dineroj1").html("Dinero: $" + jugador.getdinero());
       $("#apuestaj1").html(apostado);
     }
     else {
-      $("#dineroj2").html("Dinero: $" + jugadoractual.getdinero());
+      $("#dineroj2").html("Dinero: $" + jugador.getdinero());
       $("#apuestaj2").html(apostado);
     }
 }
 
 
 function apostar (num){/*apuesta al numero pasado (negro,rojo,par,impar,mayor,menor estan representados con numeros) y quita el dinero correspondiente*/
-  jugadoractual.añadirapuesta(num);
-  jugadoractual.modificredito(-1);
-  mostrar();
+  if (jugadoractual.getdinero() >  0) {
+    jugadoractual.añadirapuesta(num);
+    jugadoractual.modificredito(-1);
+    mostrar(jugadoractual);
+  }
+  else {
+    alert("NO CUENTA CON DINERO SUFICIENTE");
+  }
 }
 
 
@@ -148,13 +163,65 @@ function ganador(){ /*calcula numero ganador*/
   return Math.floor( Math.random ()*11);
 }
 
-function esganador(){
+function pagar(jug,val){
+  val = val*2;
+  jug.modificredito(val);
+  mostrar(jug);
+}
 
+function esganador (jugador,esgan){
+  var apu= jugador.getapuesta();
+  for (var i = 0; i < apu.length; i++) {
+    if (i === esgan.getvalor()) {
+      pagar (jugador,apu[i]);
+    }
+    else if (i === 11){
+      if (esgan.espar()) {
+        pagar (jugador,apu[i]);
+      }
+    }
+    else if (i === 12){
+      if (esgan.espar() === false) {
+        pagar (jugador,apu[i]);
+      }
+    }
+    else if (i === 13){
+      if (esgan.esmenor()) {
+        pagar (jugador,apu[i]);
+      }
+    }
+    else if (i === 14){
+      if (esgan.esmenor() === false) {
+        pagar (jugador,apu[i]);
+      }
+    }
+    else if (i === 15){
+      if (esgan.esrojo() === false) {
+        pagar (jugador,apu[i]);
+      }
+    }
+    else if (i === 16){
+      if (esgan.esrojo()) {
+        pagar (jugador,apu[i]);
+      }
+    }
+  }
+  jugador.vaciarapuesta();
+  mostrar(jugador);
+}
+
+
+function songanadores(){
+  var esgan = new numero(ganador());
+  alert("NUMERO GANADOR:  " + esgan.getvalor());
+  esganador(Mario,esgan);
+  esganador(Luigi,esgan);
 }
 
 function vincular(i){ /*vincula los botones del tablero con su respectiva apuesta*/
    botones[i].onclick = function(){apostar(i)};
 }
+
 
 /* -----------------------------------------------------------------------eventos---------------------------------------------------*/
 var Mario= new jugador;
@@ -162,13 +229,11 @@ var Luigi= new jugador;
 var jugadoractual = Mario;
 $("#j1 .paneljug").css("box-shadow", "0px 0px 40px white");
 
-
-
-
-
-$("#cambiar").on("click", cambiarjug);
-
 var botones = $(".mesa button");
 for (var i = 0; i < botones.length; i++) {
   vincular(i);
 }
+
+$("#tirar").on("click", function(){ songanadores()});
+
+$("#cambiar").on("click", cambiarjug);
