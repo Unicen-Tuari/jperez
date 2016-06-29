ec919d5fd64deb40196e157216399a15dfbfbbf4
jereprz94 <jereprz1994@gmail.com>
2016-06-29 16:31:40 -0300

    probandooooo

--- js/comportamiento.js
+++ js/comportamiento.js
@@ -7,7 +7,7 @@ function Mostrar(data) {
     cargarprod();
   })
 
-  $(".guitarrasx").on("click" = function(){
+  $(".guitarrasx").on("click", function(){
     CargarAjax("html/guitarraSX.html")
   })
   mostrarprod()
