d8071abb767532a256fc95d293f3c10931636616
jereprz94 <jereprz1994@gmail.com>
2016-06-29 13:19:32 -0300

    closes #26, cree pagina contacto, tabla en proceso

--- js/comportamiento.js
+++ js/comportamiento.js
@@ -7,7 +7,7 @@ function Mostrar(data) {
 
 function MostrarError(jqXHR, textStatus, errorThrown) {
   alert("La pagina no se puede cargar");
-  $("#articulo").html("servidor caido");
+  $("#articulo").html("SINB SERVICIO");
 }
 
 function CargarAjax(link) {
@@ -22,12 +22,59 @@ function CargarAjax(link) {
   );
 }
 
+function cargarprod(){
+  var grupo = 47;
+  var prod = {
+    codigo: null,
+    descripcion: null,
+    precio: null
+  };
+  var datos = $(".dato");
+  for (var i = 0; i < datos.length; i++) {
+    if (datos[i].value == "") {
+      alert("FALTAN CARGAR DATOS")
+    }
+  }
+  prod.codigo = inputs[0].value;
+  prod.descripcion = inputs[1].value;
+  prod.precio = inputs[2].value;
+  var informacion = {
+    "group": grupo,
+    "thing": prod
+  };
+  $.ajax({
+    method: "POST",
+    dataType: 'JSON',
+    data: JSON.stringify(informacion),
+    contentType: "application/json; charset=utf-8",
+    url: "http://web-unicen.herokuapp.com/api/create",
+    success: function(resultData){
+      $("#guardarAlert").removeClass("alert-danger")
+      $("#guardarAlert").addClass("alert-success")
+      //como le dimos dataType:"JSON" el resultData ya es un objeto
+      //la estructura que devuelve es especifica de cada servicio que usemos
+      $("#guardarAlert").html("Guardado");
+      console.log(resultData.information);
+      getInformationByGroup();
+    },
+    error:function(jqxml, status, errorThrown){
+      console.log(errorThrown);
+      $("#guardarAlert").addClass("alert-danger");
+      $("#guardarAlert").html("Error por favor intente mas tarde");
+    }
+  },
+  $("#guardarAlert").html("Cargando...")
+);
+}
+
+
+
+
 
 
 $(document).ready(function(){
   CargarAjax("html/principal.html");
-  }
-)
+})
 
 $("#principal").on("click", function(){
   CargarAjax("html/principal.html")
@@ -35,4 +82,12 @@ $("#principal").on("click", function(){
 
 $("#quienes").on("click", function(){
   CargarAjax("html/quienes.html")
+})
+
+$("#contacto").on("click", function(){
+  CargarAjax("html/contacto.html")
+})
+
+$("#guitarras").on("click", function(){
+  CargarAjax("html/guitarras.html")
 })
