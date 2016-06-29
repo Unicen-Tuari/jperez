edeef15463e210028e08d29f4dc588956e2d4eee
jereprz94 <jereprz1994@gmail.com>
2016-06-29 10:20:27 -0300

    probando ajax

---
+++ js/comportamiento.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// código de inicialización de eventos
+function Mostrar(data) {
+  $("#infoamostrar").html(data);
+}
+
+function MostrarError(jqXHR, textStatus, errorThrown) {
+  alert("La pagina no se puede cargar");
+  $("#infoamostrar").html("servidor caido");
+}
+
+function CargarAjax(link) {
+  $.ajax(
+    {
+      type:"GET",
+      url: link,
+      success: Mostrar,
+      dataType: "html",
+      error: MostrarError
+    }
+  );
+}
+
+
+
+$(document).ready(function(){
+  CargarAjax("../html/principal.html");
+  }
+)
